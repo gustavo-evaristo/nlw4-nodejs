deb950f6b52071f2228306cf8bf8bcfed431a317
Gustavo Evaristo <gug.henri1@gmail.com>
2021-04-10 15:26:04 -0300

    Iniciando o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
+import 'reflect-metadata'
+
+import './database'
+
 import express from 'express';
 
-const app = express();
+import { router }  from './routes'
 
-app.listen(3333, () => console.log('server is running'))
+const app = express()
+
+app.use(express.json())
+
+app.use(router)
+
+app.listen(3000)
